5ad7e60852241804ac6b2dc915ca3f79c2f54036
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-08-30 17:28:18 +0300

    added files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-import './sass/main.scss';
+import './styles.css';
+import './js/menu';
---
+++ src/js/menu.js
@@ -0,0 +1,3 @@
+import Handlebars from 'handlebars';
+import menuData from '../menu.json';
+import makeMenuMarkup from '../templates/menu_item.hbs';
