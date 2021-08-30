90be10d79cff6e05a88ee23a0f1be240edb208ac
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-08-30 20:02:02 +0300

    added menu items

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,9 @@
 import Handlebars from 'handlebars';
 import menuData from '../menu.json';
 import makeMenuMarkup from '../templates/menu_item.hbs';
+
+const contentRef = document.querySelector('.js-menu');
+console.log(contentRef);
+
+contentRef.innerHTML = makeMenuMarkup(menuData, Handlebars);
+console.log(contentRef);
