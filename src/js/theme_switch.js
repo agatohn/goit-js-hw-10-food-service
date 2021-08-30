81f06964e20da9fbd44acda0b12769b33d54eeed
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-08-30 21:05:44 +0300

    theme switch works

--- src/js/theme_switch.js
+++ src/js/theme_switch.js
@@ -1,6 +1,14 @@
 const switchCheckbox = document.querySelector('#theme-switch-toggle');
 switchCheckbox.addEventListener('change', onCheckboxSwitchClick);
+const bodyItem = document.querySelector('body');
 
 function onCheckboxSwitchClick() {
-    body.classList.toggle(Theme);
+   if ((!bodyItem.classList.contains('light-theme'))
+       && (!bodyItem.classList.contains('dark-theme')))
+        {bodyItem.classList.add('dark-theme');
+        return;
+    }   
+             
+    bodyItem.classList.toggle('light-theme');
+    bodyItem.classList.toggle('dark-theme');
 }
