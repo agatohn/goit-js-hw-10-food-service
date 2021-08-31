8ec75a65b463e11b70c53cc2dbacaf3b76fe9109
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-08-31 17:05:59 +0300

    finished LS theme check

--- src/js/theme_switch.js
+++ src/js/theme_switch.js
@@ -1,14 +1,42 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const CURRENT_THEME = 'theme';
 const switchCheckbox = document.querySelector('#theme-switch-toggle');
 switchCheckbox.addEventListener('change', onCheckboxSwitchClick);
-const bodyItem = document.querySelector('body');
+const bodyItemClassList = document.querySelector('body').classList;
 
-function onCheckboxSwitchClick() {
-   if ((!bodyItem.classList.contains('light-theme'))
-       && (!bodyItem.classList.contains('dark-theme')))
-        {bodyItem.classList.add('dark-theme');
-        return;
-    }   
-             
-    bodyItem.classList.toggle('light-theme');
-    bodyItem.classList.toggle('dark-theme');
+function onLoad() {
+  const themeFromLs = localStorage.getItem(CURRENT_THEME);
+  console.log(themeFromLs);
+
+  if (themeFromLs === Theme.DARK) {
+    updateTheme(Theme.DARK);
+    updateLSTheme(Theme.DARK);
+    switchCheckbox.checked = true;
+    return;
+  } else {
+    updateTheme(Theme.LIGHT);
+    updateLSTheme(Theme.LIGHT);
+    return;
+  }
+}
+onLoad();
+function onCheckboxSwitchClick(e) {
+  if (e.target.checked) {
+    updateTheme(Theme.DARK, Theme.LIGHT);
+    updateLSTheme(Theme.DARK);
+    return;
+  }
+
+  updateTheme(Theme.LIGHT, Theme.DARK);
+  updateLSTheme(Theme.LIGHT);
+}
+function updateTheme(add, remove) {
+  bodyItemClassList.add(add);
+  bodyItemClassList.remove(remove);
+}
+function updateLSTheme(theme) {
+  localStorage.setItem(CURRENT_THEME, theme);
 }
